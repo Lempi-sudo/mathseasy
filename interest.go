49360112cb734f28615da86e517de38c66ae95c8
LapshovAV <andrey.lapshov@myoffice.team>
2024-04-18 12:43:33 +0400

    2.0

--- interest.go
+++ interest.go
@@ -1,16 +1,19 @@
 package interestCalc
 
 import (
+	"fmt"
 	"math"
 )
 
 // CalculateCompoundInterest рассчитывает сложный процент для заданных параметров.
-func CalculateCompoundInterest(principal float64, rate float64, time float64) float64 {
+func CalculateCompoundInterest(principal, rate, time float64) float64 {
 	amount := principal * math.Pow((1+rate/100), time)
 	return amount - principal
 }
 
-func SimplCalc(principal float64, rate float64, time float64) float64 {
-	amount := principal * math.Pow((1+rate/100), time)
-	return amount - principal
+// Message формирует сообщение о сложном проценте на основе переданных параметров.
+// Принимает начальную сумму (principal), годовую ставку (rate) и количество лет (time).
+// Возвращает строку с сообщением о сложном проценте.
+func Message(interest, time float64) string {
+	return fmt.Sprintf("Сложный процент за %.1f лет: %.2f", time, interest)
 }
